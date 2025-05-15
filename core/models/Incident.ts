ff58dc16e1c39ba65a52fd7a2f7a60ea706953cf
juliocnc19 <juliocnietoc19@gmail.com>
2025-05-14 21:47:19 -0400

    [ADD] avances de declaracion de modelos

---
+++ core/models/Incident.ts
@@ -0,0 +1,17 @@
+import Attachment from "./Attachment"
+import Status from "./Status"
+import User from "./User"
+
+export default interface Incident {
+  id: number
+  title: string
+  description: string
+  status_id: 1 | 2 | 3 | 4
+  response: string
+  user_id: number
+  user: User
+  created_at: string
+  updated_at: string
+  attachment: Attachment[]
+  status: Status
+}
---
+++ core/models/User.ts
@@ -0,0 +1,16 @@
+import { Role } from "react-native"
+
+export default interface User {
+  id: number
+  first_name: string
+  last_name: string
+  cedula:string
+  email: string
+  username: string
+  password: string
+  role_id: string
+  created_at: string
+  updated_at: string
+  token:string
+  role:Role
+}

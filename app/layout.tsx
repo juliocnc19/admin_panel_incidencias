e2d7f8180199396ebecc10c29683691e6527c6ab
juliocnc19 <juliocnietoc19@gmail.com>
2025-06-30 16:16:07 -0400

    [ADD] uso del logo de ais

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Panel Administrativo",
   description: "Panel administrativo moderno",
+  icons: {
+    icon: '/logo_ais.png',
+  },
 }
 
 export default function RootLayout({
